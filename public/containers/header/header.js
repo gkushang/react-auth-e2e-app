55a333a6943daa0de694ebf5e6ac5dc30f1c0823
Kushang Gajjar <g.kushang@gmail.com>
2016-11-01 15:06:31 -0400

    header

--- public/containers/header/header.js
+++ public/containers/header/header.js
@@ -7,6 +7,7 @@ export default function() {
                 <div className="container-fluid">
 
                     <div className="navbar-inner">
+
                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                             <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"> </span>
@@ -14,14 +15,30 @@ export default function() {
                             <span className="icon-bar"> </span>
                         </button>
 
-                        <a className="navbar-brand navbar-default" href="#">E2E authnodeweb</a>
+                        <a className="navbar-brand navbar-default" href="https://github.paypal.com/Identity-R/authnodeweb" target="_blank">
+                            <i className="fa fa-github">  </i>
+                            E2E authnodeweb
+                        </a>
+
                     </div>
 
                     <div id="navbar" className="navbar-collapse collapse">
                         <ul className="nav navbar-nav navbar-right">
-                            <li className="text-info">
+                            <li>
                                 <a href="https://github.paypal.com/kugajjar/authuserserv/blob/master/readme.md" target="_blank">
                                     API Docs
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="http://authserv-8375.ccg21.dev.paypalcorp.com:8800/clientAuth" target="_blank">
+                                    PN Simulator
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="http://cukes-3821.ccg21.dev.paypalcorp.com/projects/authnodeweb/" target="_blank">
+                                    Cucumber Docs
                                 </a>
                             </li>
 
