40b7e426edd50d75b11c51c1e516cb0d7cd80b55
Kushang Gajjar <g.kushang@gmail.com>
2016-11-01 15:09:19 -0400

    input

--- public/containers/presentational/Input.js
+++ public/containers/presentational/Input.js
@@ -7,7 +7,7 @@ export default (field) => {
             <TextField
                 floatingLabelText={field.placeholder}
                 errorText = {field.meta.touched && field.meta.error}
-                className="security-field common small-font"
+                className="common small-font"
                 errorStyle={{"font-size": "10px"}}
                 hintStyle={{"font-size": "16px"}}
                 floatingLabelStyle={{"font-size": "16px"}}
